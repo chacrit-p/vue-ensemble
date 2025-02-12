835a28984b899d354be7337d534f903d252bdd03
chacrit-p <119285304+chacrit-p@users.noreply.github.com>
2025-02-12 12:59:13 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import "axios";
+import { ref, onMounted } from "vue";
+
+const BASE_URL = import.meta.env.VITE_BASE_API_URL;
+
+const predictState = ref(null);
+const loadingState = ref(false);
+const formState = ref({
+  gender: 0,
+  age: 0,
+});
+
+const predict = () => {
+  loadingState.value = true;
+  axios
+    .post(`${BASE_URL}/predict`, {})
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      loadingState.value = false;
+    });
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="p-5 min-h-screen">
+    <div class="max-w-2xl mx-auto">
+      <h1 class="text-3xl font-bold text-center leading-loose mb-4">
+        Student Depression Predict 😁🧑‍🎓
+      </h1>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
